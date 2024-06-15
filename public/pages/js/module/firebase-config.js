af073dc13f2c18b7d298569d03841d8125fd2803
Ozan Lago-Sereflioglu <lago.ozan@gmail.com>
2024-06-14 20:48:50 -0400

    Added backend files

---
+++ public/pages/js/module/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQDIXyT8lMN2ewQdxENJ44i4_dxvY807I",
+    authDomain: "quirky-website.firebaseapp.com",
+    databaseURL: "https://quirky-website-default-rtdb.firebaseio.com",
+    projectId: "quirky-website",
+    storageBucket: "quirky-website.appspot.com",
+    messagingSenderId: "95660131677",
+    appId: "1:95660131677:web:3b50fed872ef89ab96d4b4"
+  };
+  
